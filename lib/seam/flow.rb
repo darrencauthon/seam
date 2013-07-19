e1539134a0b1f822b926eb0eab5ac9f9710b8995
Darren Cauthon <darren@cauthon.com>
2013-07-19 16:16:20 -0500

    Starting to work on the setup of a flow.

--- lib/seam/flow.rb
+++ lib/seam/flow.rb
@@ -1,4 +1,13 @@
 module Seam
   class Flow
+    attr_reader :steps
+
+    def initialize
+      @steps = []
+    end
+
+    def method_missing(meth, *args, &blk)
+      @steps << Object.new
+    end
   end
 end
--- spec/seam/flow_spec.rb
+++ spec/seam/flow_spec.rb
@@ -1,7 +1,23 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Seam::Flow do
-  it "should exist" do
-    true.must_equal true
+  describe "empty" do
+    let(:flow) { Seam::Flow.new }
+
+    it "should have an empty set of steps" do
+      flow.steps.count.must_equal 0
+    end
+  end
+
+  describe "one action setup" do
+    let(:flow) do
+                 flow = Seam::Flow.new
+                 flow.do_something
+                 flow
+               end
+
+    it "should return one step" do
+      flow.steps.count.must_equal 1
+    end
   end
 end
