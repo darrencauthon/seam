0aa662dcf6208b0db654da04445877ca448f5548
Darren Cauthon <darren@cauthon.com>
2013-08-06 11:23:20 -0500

    Refactor specs.

--- spec/seam/worker_spec.rb
+++ spec/seam/worker_spec.rb
@@ -6,24 +6,26 @@ describe "worker" do
     test_moped_session['efforts'].drop
   end
 
-  it "should work" do
-    flow = Seam::Flow.new
-    flow.apple
-    flow.orange
+  describe "move_to_next_step" do
+    it "should work" do
+      flow = Seam::Flow.new
+      flow.apple
+      flow.orange
 
-    effort = flow.start( { first_name: 'John' } )
+      effort = flow.start( { first_name: 'John' } )
 
-    effort.next_step.must_equal "apple"
+      effort.next_step.must_equal "apple"
 
-    apple_worker = Seam::Worker.new
-    apple_worker.for(:apple)
-    def apple_worker.process
-      move_to_next_step
+      apple_worker = Seam::Worker.new
+      apple_worker.for(:apple)
+      def apple_worker.process
+        move_to_next_step
+      end
+
+      apple_worker.execute effort
+
+      effort = Seam::Effort.find(effort.id)
+      effort.next_step.must_equal "orange"
     end
-
-    apple_worker.execute effort
-
-    effort = Seam::Effort.find(effort.id)
-    effort.next_step.must_equal "orange"
   end
 end
