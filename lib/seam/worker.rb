02b9300640604ef1911fbda4575d18519c08f191
Darren Cauthon <darren@cauthon.com>
2013-08-06 11:31:22 -0500

    Implement try_again_in seconds.

--- lib/seam/worker.rb
+++ lib/seam/worker.rb
@@ -13,5 +13,10 @@ module Seam
       @current_effort.next_step = @current_effort.flow[:steps].last[:name]
       @current_effort.save
     end
+
+    def try_again_in seconds
+      @current_effort.next_execute_at = Time.now + seconds
+      @current_effort.save
+    end
   end
 end
--- spec/seam/worker_spec.rb
+++ spec/seam/worker_spec.rb
@@ -6,6 +6,10 @@ describe "worker" do
     test_moped_session['efforts'].drop
   end
 
+  after do
+    Timecop.return
+  end
+
   describe "move_to_next_step" do
     it "should work" do
       flow = Seam::Flow.new
@@ -26,6 +30,42 @@ describe "worker" do
 
       effort = Seam::Effort.find(effort.id)
       effort.next_step.must_equal "orange"
+    end
+  end
+
+  describe "try_again_in" do
+
+    let(:effort) do
+      flow = Seam::Flow.new
+      flow.apple
+      flow.orange
+
+      flow.start( { first_name: 'John' } )
+    end
+
+    before do
+      Timecop.freeze Time.parse('3/4/2013')
+
+
+      effort.next_step.must_equal "apple"
+
+      apple_worker = Seam::Worker.new
+      apple_worker.for(:apple)
+      def apple_worker.process
+        try_again_in 1.day
+      end
+
+      apple_worker.execute effort
+    end
+
+    it "should not update the next step" do
+      fresh_effort = Seam::Effort.find(effort.id)
+      fresh_effort.next_step.must_equal "apple"
+    end
+
+    it "should not update the next execute date" do
+      fresh_effort = Seam::Effort.find(effort.id)
+      fresh_effort.next_execute_at.must_equal Time.parse('4/4/2013')
     end
   end
 end
