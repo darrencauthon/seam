9f892bfba5d4bf7b4491f9dfb9be37a3a12aa611
Darren Cauthon <darren@cauthon.com>
2013-11-21 22:17:24 -0600

    Refactor for readability.

--- lib/seam/wait_worker.rb
+++ lib/seam/wait_worker.rb
@@ -5,9 +5,17 @@ module Seam
     end
 
     def process
-      if effort.history.count == 0 || effort.history.last[:step] != 'wait'
-        try_again_in current_step[:arguments][0]
-      end
+      try_again_in the_appropriate_amount_of_time if we_should_wait
+    end
+
+    private
+
+    def we_should_wait
+      effort.history.count == 0 || effort.history.last[:step] != 'wait'
+    end
+
+    def the_appropriate_amount_of_time
+      current_step[:arguments][0]
     end
   end
 end
