66b3c298ffc028ccd94bfa013277606d77a668c8
Darren Cauthon <darren@cauthon.com>
2013-11-21 21:37:35 -0600

    Refactor the specs.

--- spec/seam/wait_worker_spec.rb
+++ spec/seam/wait_worker_spec.rb
@@ -10,48 +10,52 @@ describe Seam::WaitWorker do
     Seam::WaitWorker.new.step.must_equal 'wait'
   end
 
-  [:length_of_time].to_objects { [
-    [3.days],
-    [1.day],
-    [30.minutes]
-  ] }.each do |test|
-    describe "using it in a flow" do
+  describe "using it in a flow" do
 
-      let(:flow) do
-        f = Seam::Flow.new
-        f.wait test.length_of_time
-        f.do_something
-        f
+    let(:effort_id) do
+      effort = flow.start
+      effort.id
+    end
+
+    let(:today) do
+      Time.parse '1/1/2011'
+    end
+
+    [:length_of_time].to_objects { [
+      [3.days],
+      [1.day],
+      [30.minutes]
+    ] }.each do |test|
+
+      describe "a simple situation" do
+
+        let(:flow) do
+          f = Seam::Flow.new
+          f.wait test.length_of_time
+          f.do_something
+          f
+        end
+
+        before do
+          Timecop.freeze today
+          effort_id
+        end
+
+        it "should immediately wait" do
+          Seam::WaitWorker.new.execute_all
+          Seam::Effort.find(effort_id).next_step.must_equal "wait"
+        end
+
+        it "should continue on after the days have passed" do
+          Seam::WaitWorker.new.execute_all
+
+          Timecop.freeze(today + test.length_of_time)
+
+          Seam::WaitWorker.new.execute_all
+          Seam::Effort.find(effort_id).next_step.must_equal "do_something"
+        end
+
       end
-
-      let(:effort_id) do
-        effort = flow.start
-        effort.id
-      end
-
-      let(:today) do
-        Time.parse '1/1/2011'
-      end
-
-      before do
-        Timecop.freeze today
-        effort_id
-      end
-
-      it "should immediately wait" do
-        Seam::WaitWorker.new.execute_all
-        Seam::Effort.find(effort_id).next_step.must_equal "wait"
-      end
-
-      it "should continue on after the days have passed" do
-        Seam::WaitWorker.new.execute_all
-
-        Timecop.freeze(today + test.length_of_time)
-
-        Seam::WaitWorker.new.execute_all
-        Seam::Effort.find(effort_id).next_step.must_equal "do_something"
-      end
-
     end
   end
 end
