9696acda0dae13ed7e014ec5a07124a7d3721dce
Darren Cauthon <darren@cauthon.com>
2013-11-22 10:06:20 -0600

    Cleanup.

--- lib/seam/persistence.rb
+++ lib/seam/persistence.rb
@@ -4,18 +4,12 @@ module Seam
       effort = Seam::InMemory.records.select { |x| x.id == effort_id }.first
       return nil unless effort
       effort.clone
-      #document = Seam::MongoDb.collection.find( { id: effort_id } ).first
-      #return nil unless document
-      #Seam::Effort.parse document
     end
 
     def self.find_all_pending_executions_by_step step
       Seam::InMemory.records
         .select { |x| x.next_step == step && x.next_execute_at <= Time.now }
         .map { |x| x.clone }
-      #Seam::MongoDb.collection
-        #.find( { next_step: step, next_execute_at: { '$lte' => Time.now } } )
-        #.map { |x| Seam::Effort.parse x }
     end
 
     def self.save effort
@@ -24,13 +18,10 @@ module Seam
         Seam::InMemory.records = Seam::InMemory.records.select { |x| x.id != effort.id }.to_a
       end
       create effort
-      #Seam::MongoDb.collection.find( { id: effort.id } )
-      #    .update("$set" => effort.to_hash)
     end
 
     def self.create effort
       Seam::InMemory.records = [Seam::InMemory.records, effort].flatten
-      #Seam::MongoDb.collection.insert(effort.to_hash)
     end
 
     def self.all
