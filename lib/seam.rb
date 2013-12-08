91b757638b803c33e3f2b4cb4a82968473a0722e
Darren Cauthon <darren@cauthon.com>
2013-12-08 17:35:48 -0600

    Add method that returns steps that need to be executed.

--- lib/seam.rb
+++ lib/seam.rb
@@ -6,4 +6,11 @@ require_relative 'seam/worker'
 Dir[File.dirname(__FILE__) + '/seam/*.rb'].each {|file| require file }
 
 module Seam
+
+  def self.steps_to_run
+    Seam::Persistence.find_something_to_do
+                       .group_by { |x| x.next_step }
+                       .map      { |x| x[0] }
+  end
+
 end
